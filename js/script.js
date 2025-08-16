f48d84f72a3523e39e6d9bbceb00deeb3e0f9ae6
eme-nguema <marioidrisondo@hotmail.com>
2025-08-16 22:34:34 +0100

    Actualiza los cambios
    correction de errores de vista movil.

--- js/script.js
+++ js/script.js
@@ -2,14 +2,13 @@ let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
-  menu.classList.toggle('bx-x');
-  navbar.classList.toggle('active');
-
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 }
 
 window.onscroll = () => {
-  menu.classList.remove('bx-x');
-  navbar.classList.remove('active');
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
 
 const typed = new Typed('.multiple-text', {
